6f5a9bceea5cc75d7d233b1d76e13076866955ee
Stephen <stephen_dade@hotmail.com>
2017-03-18 12:59:28 +1100

    APM Install - uncomment git clones

--- apm_install.sh
+++ apm_install.sh
@@ -4,12 +4,12 @@
 
 python -m ensurepip --user
 python -m pip install --user future
-#git clone git://github.com/ArduPilot/ardupilot.git
-#git clone git://github.com/tridge/jsbsim.git
+git clone git://github.com/ArduPilot/ardupilot.git
+git clone git://github.com/tridge/jsbsim.git
 cd jsbsim
-#./autogen.sh
-#make
-#cp src/JSBSim.exe /usr/local/bin
+./autogen.sh
+make
+cp src/JSBSim.exe /usr/local/bin
 cd ../ardupilot
 git submodule update --init --recursive
 ./modules/waf/waf-light configure --board=sitl
